ae1e6c6386c4aea4003daf2206bdfa0ed2083302
ocorrenciasidr <marcelocardes@prof.educacao.sp.gov.br>
2025-10-06 08:02:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -351,4 +351,7 @@ def api_alunos_por_sala(sala):
 
 # -------------------------- Run --------------------------
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
    
+
